050e641bc44100e2b11d26b24e7247e96ebdc914
mcerruti0311 <mcerruti@sis.us>
2018-05-04 17:04:33 -0700

    Added SQL Files

---
+++ Queries/payroll.sql
@@ -0,0 +1,21 @@
+Select Employee__c,
+  Employee__r.Name,
+  Related_UTM__r.Location_Captured__r.Parent_Location__r.Name Parent,
+  Week_Ending__c,
+  SUM(RT__c),
+  SUM(OT__c),
+  SUM(DT__c),
+  SUM(Total__c)
+FROM
+  Payroll__c
+WHERE
+  Void__c = FALSE
+  AND Related_UTM__r.Location_Captured__r.Parent_Location__c = '{pv0}'
+  AND RecordType.Name = 'Payroll Punch'
+  AND Salary_or_Hourly__c = 'Hourly'
+  AND Week_Ending__c = THIS_WEEK
+GROUP BY
+  Employee__c,
+  Employee__r.Name,
+  Related_UTM__r.Location_Captured__r.Parent_Location__r.Name,
+  Week_Ending__c
